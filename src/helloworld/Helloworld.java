4db04100efffd168eb1b626730c6333fba49d08c
huyuyuan <huyuyuan@pc-hz20082808.hikvision.com>
2018-10-15 14:36:58 +0800

    testsetsetstsetststset

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -5,5 +5,6 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello git world!!!");
 		System.out.println("update");
+		System.out.println("test");
 	}
 }
